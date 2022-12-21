979b927bdda8d298618905fe238db35fc0068afc
Strathan <121134668+Strathan@users.noreply.github.com>
2022-12-21 18:52:11 +1300

    Update streamlit_app.py
    format

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,21 +38,6 @@ try:
 except URLError as e:
     streamlit.error()
 
-
-#------
-
-#streamlit.header("Fruityvice Fruit Advice!")
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-# Normalize the json
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Output json as a table
-#streamlit.dataframe(fruityvice_normalized)
-
 # Dont run past here
 streamlit.stop()
 
